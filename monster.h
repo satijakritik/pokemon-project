e2888c8c44ccc6134efaf61606d2a4db22a82248
Shriya-Bedi <63390035+Shriya-Bedi@users.noreply.github.com>
2020-04-27 15:59:43 +0530

    Create monster.h

---
+++ monster.h
@@ -0,0 +1,15 @@
+// header file for monster.cpp
+// includes class
+
+#include <iostream>
+#include “main.h”     //to be explained created later, for main compilation
+
+using namespace std;
+class Monster
+{
+  string name;
+  int health;
+  int damage;
+  Monster (string newName, int newhealth, int newdamage); // constructor function
+  void attack (&Player);
+};
