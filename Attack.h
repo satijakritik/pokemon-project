2513a690cb9abd3116c19746ad2ed9ea5e75934b
Shriya-Bedi <63390035+Shriya-Bedi@users.noreply.github.com>
2020-04-27 17:50:14 +0530

    Create Attack.h

---
+++ Attack.h
@@ -0,0 +1,17 @@
+// main attack header file
+
+#include <iostream>
+using namespace std;
+
+#include "Player.h"
+#include "Monster.h"
+
+class Attack
+{
+  public:
+		Monster &M;
+		Attack (Monster &newM);
+    
+    // implement functions for type of attack / attack rules
+		
+};
