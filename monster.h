7b5851363c6a71576c4f14bad82d6c44fd1c408d
Shriya-Bedi <63390035+Shriya-Bedi@users.noreply.github.com>
2020-05-09 13:24:27 +0530

    Update monster.h

--- monster.h
+++ monster.h
@@ -1,7 +1,5 @@
 // header file for monster.cpp
-// includes class
 
-//#include “main.h”     //to be created later, for main compilation
 #ifndef MONSTER_H
 #define MONSTER_H
 
@@ -13,9 +11,19 @@ class Monster
     int health;
     int move1_damage;
     int move2_damage;
+    // two integer variables to track damage dealed in each player's move
+  
     Monster (string newName, int newhealth, int newmove1damage, int newmove2damage); // constructor function
     void attack (Monster Monster1, Monster &Monster2, int move_choice, bool game_over, vector<Monster> &player1_monster_list, vector<Monster> &player2_monster_list, string current_player);
-
+    // calls main attack function 
+  
 };
 
 #endif
+
+
+
+
+
+
+
